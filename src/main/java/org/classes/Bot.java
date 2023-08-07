25631b1201d91cdbedc903ace39ffef079e2c469
zanzelika <zanzelika@yandex.ru>
2023-08-07 14:21:38 +0300

    Так, маленько тут всего добавила,  в основном тут с примера добавляла. Статья: https://habr.com/ru/articles/528694/ Думаю, нам пригодиться.

---
+++ src/main/java/org/classes/Bot.java
@@ -0,0 +1,71 @@
+import lombok.Getter;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class Bot extends TelegramLongPollingCommandBot {
+    private final String BOT_NAME;
+    private final String BOT_TOKEN;
+
+    @Getter
+    private static final Settings defaultSettings = new Settings(1, 15, 1);
+
+    @Getter
+    private static Map<Long, Settings> userSettings;
+
+    public Bot(String botName, String botToken) {
+        super();
+        this.BOT_NAME = botName;
+        this.BOT_TOKEN = botToken;
+        //создаём вспомогательный класс для работы с сообщениями, не являющимися командами
+        this.nonCommand = new NonCommand();
+        //регистрируем команды
+        register(new StartCommand("start", "Старт"));
+        register(new HelpCommand("help","Помощь"));
+        userSettings = new HashMap<>();
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+    @Override
+    public void processNonCommandUpdate(Update update) {
+        Message msg = update.getMessage();
+        Long chatId = msg.getChatId();
+        String userName = getUserName(msg);
+
+        String answer = nonCommand.nonCommandExecute(chatId, userName, msg.getText());
+        setAnswer(chatId, userName, answer);
+    }
+    public static Settings getUserSettings(Long chatId) {
+        Map<Long, Settings> userSettings = Bot.getUserSettings();
+        Settings settings = userSettings.get(chatId);
+        if (settings == null) {
+            return defaultSettings;
+        }
+        return settings;
+    }
+
+    private void setAnswer(Long chatId, String userName, String text) {
+        SendMessage answer = new SendMessage();
+        answer.setText(text);
+        answer.setChatId(chatId.toString());
+        try {
+            execute(answer);
+        } catch (TelegramApiException e) {
+            // ....
+        }
+    }
+}
